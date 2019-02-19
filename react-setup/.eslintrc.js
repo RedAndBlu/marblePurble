929d67c980ec5de5278c270400f9c4e61a537ae8
RedAndBlue <academyckc@gmail.com>
2019-02-19 20:58:07 +0100

    added a new eslintcr file with relative packages to lint react jsx in the react-setup

--- react-setup/.eslintrc.js
+++ react-setup/.eslintrc.js
@@ -1,10 +1,43 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "env": {
-      "browser": true
-    },
-    "rules": {
-        "class-methods-use-this": "off",
-        'semi': ['error', 'always'],
-    },
+	'env': {
+		'browser': true,
+		'es6': true
+	},
+	"extends": [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+	'globals': {
+		'Atomics': 'readonly',
+		'SharedArrayBuffer': 'readonly'
+	},
+	'parser': "babel-eslint",
+	'parserOptions': {
+		'ecmaFeatures': {
+			'jsx': true
+		},
+		'ecmaVersion': 2018,
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'react'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'windows'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
 };
--- react-setup/src/js/index.js
+++ react-setup/src/js/index.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import '../scss/style.scss';
 
-console.log('tes me');
+const testElement = <div>It is working</div>;
+
+ReactDOM.render(testElement, document.getElementById('root'));
