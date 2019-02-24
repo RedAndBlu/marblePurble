fe36cb0e11ed0f8f730594172eaeda7cadec9e71
RedAndBlue <academyckc@gmail.com>
2019-02-24 19:00:45 +0100

    add dependency optimize-css-assets-webpack-plugin
    change webpack prodaction config now css is minified

--- react-setup/src/js/index.js
+++ react-setup/src/js/index.js
@@ -4,4 +4,8 @@ import '../scss/style.scss';
 
 const testElement = <div>It is working</div>;
 
+function champ() {
+  const p = 10;
+}
+
 ReactDOM.render(testElement, document.getElementById('root'));
